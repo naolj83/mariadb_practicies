1283d0fb1027d823fe2155030a2289e8a330fe87
BIT <BIT@DESKTOP-OTC3N9B>
2021-05-20 12:32:55 +0900

    PreparedStatement 연습

---
+++ jdbc-practices/sql-연습.sql
@@ -0,0 +1,15 @@
+show tables;
+desc employees;
+
+select emp_no, birth_date, first_name 
+  from employees
+ where first_name like 'pat%';
+
+ -- select
+ select no, name from dept;
+
+ -- insert
+ insert into dept values(null, '총무');
+
+ -- delete
+ delete from dept where no = 3; 
