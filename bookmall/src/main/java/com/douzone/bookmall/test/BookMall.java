cfece69d48ad18674e1bdb5d840bd99f6cc5f1ac
BIT <BIT@DESKTOP-U4THS2M>
2021-05-21 12:23:25 +0900

    평가과제

---
+++ bookmall/src/main/java/com/douzone/bookmall/test/BookMall.java
@@ -0,0 +1,22 @@
+package com.douzone.bookmall.test;
+
+import com.douzone.bookmall.dao.CategoryDao;
+
+public class BookMall {
+
+	public static void main(String[] args) {
+		System.out.println("===========회원============");
+		System.out.println("===========카테고리============");
+		
+		CategoryDao categoryDao = new CategoryDao() {
+		categoryDao.insert();
+		categoryDao.insert();
+		categoryDao.insert();
+		List list = categoryDao.findAll();
+		for(CategoryVo vo : list) {
+			System.out.println(vo);
+		}
+		}
+	}
+
+}
